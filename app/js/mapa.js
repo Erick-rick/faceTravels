01fdf8a30b268f276e575664cec7deb1b98618ac
nonato dias home <nonato94@gmail.com>
2017-01-12 20:54:39 -0300

    adicao dos scripts do angularjs

--- app/js/mapa.js
+++ app/js/mapa.js
@@ -1,7 +1,17 @@
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -15.779, lng: -47.929},
-    zoom: 4
+
+	var myLatLng = {lat: -15.779, lng: -47.929};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+	    center: myLatLng,
+	    zoom: 4
     });
+
+    var marker = new google.maps.Marker({
+	    position: myLatLng,
+	    map: map,
+	    title: 'Brasilia'
+	  });
+
 }
---
+++ app/scripts/controllers/mapa/mapa-controller.js
@@ -0,0 +1,7 @@
+
+angular.module('myApp', []).controller('mapaController', function() {
+	var self = this;
+
+	self.anoFiltro = 2017;
+	
+});
