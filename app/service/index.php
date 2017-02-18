7c78da2d974203297196dc8d1e7f401a0f41b793
Jefferson Uchoa Ponte <jefponte@unilab.edu.br>
2017-02-18 19:17:06 -0300

    Inserindo testado.

--- app/service/index.php
+++ app/service/index.php
@@ -1,20 +1,16 @@
-<?php 
+<!DOCTYPE html>
+<html lang="pt-br" ng-app="mapApp">
+   <head>
+   </head>
+	<body>
+	<h1>Inserir Usuario</h1>
+	<form action="inserir_usuario.php" method="post">
+		<input type="text" name="nome" />
+		<input type="text" name="login" />
+		<input type="text" name="senha" />
+		<input type="submit" name="Enviar" />
+	</form>
+	</body>
 
-
-function __autoload($classe) {
-	if (file_exists ( 'classes/dao/' . $classe . '.php' ))
-		include_once 'classes/dao/' . $classe . '.php';
-	if (file_exists ( 'classes/model/' . $classe . '.php' ))
-		include_once 'classes/model/' . $classe . '.php';
-	if (file_exists ( 'classes/controller/' . $classe . '.php' ))
-		include_once 'classes/controller/' . $classe . '.php';
-	if (file_exists ( 'classes/util/' . $classe . '.php' ))
-		include_once 'classes/util/' . $classe . '.php';
-	if (file_exists ( 'classes/view/' . $classe . '.php' ))
-		include_once 'classes/view/' . $classe . '.php';
-}
-
-
-?>
-
+</html>
 
