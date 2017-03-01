63385c90f12fa9f69cce90d0a97a4075b03eba78
nonato dias home <nonato94@gmail.com>
2017-03-01 20:10:47 -0300

    modal remove usuario adicionado

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,6 +1,6 @@
 angular.module("mapApp").constant("config", {
 
-	//baseUrl: "http://localhost:8080/service/", 
+	//baseUrl: "http://localhost:80/service/", 
 	baseUrl: "http://facetravels.ddns.net/service/", 
 	facebookApiKey: '1789567744640995'
 
--- app/scripts/controllers/user/users-new-controller.js
+++ app/scripts/controllers/user/users-new-controller.js
@@ -20,4 +20,8 @@ angular.module('mapApp').controller('usersNewController', function(userService) 
   		});
 	}
 
+	self.remove = function(){
+		
+	}
+
 });
