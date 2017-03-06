af277a2bcee5fd9794c7b95af44948acbf7b57ef
Jefferson Uchoa Ponte <jefponte@unilab.edu.br>
2017-03-06 18:07:38 -0300

    Só facilitando os testes.

--- app/service/index.php
+++ app/service/index.php
@@ -1,21 +1,44 @@
+<?php 
+function __autoload($classe) {
+	if (file_exists ( 'classes/dao/' . $classe . '.php' ))
+		include_once 'classes/dao/' . $classe . '.php';
+		if (file_exists ( 'classes/model/' . $classe . '.php' ))
+			include_once 'classes/model/' . $classe . '.php';
+			if (file_exists ( 'classes/controller/' . $classe . '.php' ))
+				include_once 'classes/controller/' . $classe . '.php';
+				if (file_exists ( 'classes/util/' . $classe . '.php' ))
+					include_once 'classes/util/' . $classe . '.php';
+					if (file_exists ( 'classes/view/' . $classe . '.php' ))
+						include_once 'classes/view/' . $classe . '.php';
+}
+
+function exibirFormulario($arrayCampos, $action){
+	
+	echo '<form action="'.$action.'" method="post">';
+	
+	foreach($arrayCampos as $campo){
+		echo '<label for="'.$campo.'">'.$campo.'</label>';
+		echo '<input type="text" name="'.$campo.'" id="'.$campo.'"/>';
+	}
+	
+	echo '<input type="submit" name="Enviar" />
+		</form>';	
+	
+}
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br" ng-app="mapApp">
    <head>
    </head>
 	<body>
 	<h1>Inserir Usuario</h1>
-	<form action="usuario/inserir_usuario.php" method="post">
-		<input type="text" name="nome" />
-		<input type="text" name="login" />
-		<input type="text" name="senha" />
-		<input type="submit" name="Enviar" />
-	</form>
 	<?php 
-	if($_SERVER['HTTP_HOST'] == "localhost"){
-		echo 'Eh local';
-	}	
-	?>
 	
+	exibirFormulario( array('nome', 'login', 'senha'), 'usuario/inserir_usuario.php');
+	
+	?>
+
 	<h1>Autenticar</h1>
 	<form action="autenticar_usuario.php" method="post">
 		<input type="text" name="login" />
