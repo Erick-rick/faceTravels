ea7f50e074a5b11df93fa807eba837a472a38f7a
nonato dias home <nonato94@gmail.com>
2017-03-05 22:02:51 -0300

    correcao de novo usuario login

--- app/scripts/controllers/user/users-new-controller.js
+++ app/scripts/controllers/user/users-new-controller.js
@@ -1,11 +1,38 @@
-angular.module('mapApp').controller('usersNewController', function(userService, $state) {
+angular.module('mapApp').controller('usersNewController', function(userService, $rootScope, $state) {
 	var self = this;
 
 	self.usuario = {};
 
 	self.save = function(){
 		userService.saveUser(self.usuario).then(function onSuccess(response) {
-    		$state.go("dashboard.home", {mensagem: 'Olá '+ self.usuario.nome }); 
+			var login = {
+				login: self.usuario.login,
+				senha: self.usuario.senha
+			}
+
+			userService.authenticate(login).then(function onSuccess(response) {
+				var data = {
+					url: 'img/userMain.png'
+				}
+	    		if(response.data != "Errou!" && response.data != "Incompleto"){
+
+	    			var usuarioLogado = response.data[0];
+	    			usuarioLogado.largePicture = {data}; 
+
+					//Armazena o usuario na sessao
+					userService.storeUser(usuarioLogado);
+
+					$rootScope.usuario = usuarioLogado;
+
+					$state.go("dashboard.home", {mensagem: 'Olá '+ response.data[0].nome });   
+				} 
+				else{
+					showMensagem('Senha invalida');
+				}
+
+	  		}, function onError(response) {
+			   
+	  		});
   		}, function onError(response) {
 		   
   		});
