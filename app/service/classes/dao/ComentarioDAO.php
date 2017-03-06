fc5b33f7a0072402fe4b633df810b0f9830256db
Jefferson Uchoa Ponte <jefponte@unilab.edu.br>
2017-03-06 16:55:19 -0300

    Cadastrando comentário no mapa.

--- app/service/classes/dao/ComentarioDAO.php
+++ app/service/classes/dao/ComentarioDAO.php
@@ -13,9 +13,25 @@ class ComentarioDAO extends DAO{
 	}
 	public function inserir(Comentario $comentario){
 	
+		$sql = "INSERT INTO comentario(texto, id_mapa, id_usuario_autor, data)
+				VALUES(:texto, :idMapa, :usuarioAutor, :data)";
 		$texto = $comentario->getTexto();
+		$idMapa = $comentario->getMapa()->getId();
+		$usuarioAutor = $comentario->getAutor()->getId();
 		$data = date("Y-m-d H:i:s");
-		return true;
+		try {
+			$db = $this->getConexao();
+			$stmt = $db->prepare($sql);
+			$stmt->bindParam("texto", $texto, PDO::PARAM_STR);
+			$stmt->bindParam("idMapa", $idMapa, PDO::PARAM_STR);
+			$stmt->bindParam("usuarioAutor", $usuarioAutor, PDO::PARAM_STR);
+			$stmt->bindParam("data", $data, PDO::PARAM_STR);
+			$result = $stmt->execute();
+			return $result;
+		
+		} catch(PDOException $e) {
+			echo '{"error":{"text":'. $e->getMessage() .'}}';
+		}
 	
 	}
 	
