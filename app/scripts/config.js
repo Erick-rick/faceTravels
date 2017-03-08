9249575b71d7bdc46a1400ed19f9c8b8b257c47b
nonato dias home <nonato94@gmail.com>
2017-03-07 21:25:39 -0300

    versao 0.3 adicionada

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,7 +1,7 @@
 angular.module("mapApp").constant("config", {
 
-	baseUrl: "http://localhost:80/service/usuario/", 
-	//baseUrl: "http://facetravels.ddns.net/service/usuario/", 
+	//baseUrl: "http://localhost:80/service/usuario/", 
+	baseUrl: "http://facetravels.ddns.net/service/usuario/", 
 	//facebookApiKey: '1789567744640995'
 	facebookApiKey: '680697265443834'
 
